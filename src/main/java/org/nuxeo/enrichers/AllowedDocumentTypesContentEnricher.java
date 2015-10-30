ad76fba20906d453d0a036374c8005639caa7d1e
Vincent Dutat <vdutat@nuxeo.com>
2015-10-30 15:50:26 -0700

    7.x compatible

--- src/main/java/org/nuxeo/enrichers/AllowedDocumentTypesContentEnricher.java
+++ src/main/java/org/nuxeo/enrichers/AllowedDocumentTypesContentEnricher.java
@@ -1,26 +1,37 @@
 package org.nuxeo.enrichers;
 
+import static org.nuxeo.ecm.core.io.registry.reflect.Instantiations.SINGLETON;
+import static org.nuxeo.ecm.core.io.registry.reflect.Priorities.REFERENCE;
+
 import java.io.IOException;
 import java.util.Collection;
 
 import org.codehaus.jackson.JsonGenerator;
-import org.nuxeo.ecm.automation.io.services.enricher.AbstractContentEnricher;
-import org.nuxeo.ecm.automation.io.services.enricher.RestEvaluationContext;
-import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.io.marshallers.json.enrichers.AbstractJsonEnricher;
+import org.nuxeo.ecm.core.io.registry.reflect.Setup;
 import org.nuxeo.ecm.platform.types.Type;
 import org.nuxeo.ecm.platform.types.TypeManager;
 import org.nuxeo.runtime.api.Framework;
 
-public class AllowedDocumentTypesContentEnricher extends AbstractContentEnricher {
-
+//The class will be instanciated as a singleton
+//Priority defines which marshaller will be used in case of conflict. Priority is an integer.
+//The higher the number, the more priority you get: 10 > 1 for instance.
+@Setup(mode = SINGLETON, priority = REFERENCE)
+public class AllowedDocumentTypesContentEnricher extends AbstractJsonEnricher<DocumentModel> {
+    
+    // The enricher will be called using X-NXenrichers.document: allowedDocumentTypes
     public static final String ID= "allowedDocumentTypes";
 
+    public AllowedDocumentTypesContentEnricher() {
+        super(ID);
+    }
+
     @Override
-    public void enrich(JsonGenerator jg, RestEvaluationContext ec) throws ClientException, IOException {
-        DocumentModel doc = ec.getDocumentModel();
+    public void write(JsonGenerator jg, DocumentModel doc) throws IOException {
         TypeManager typeManager = Framework.getService(TypeManager.class);
         Collection<Type> subTypes = typeManager.getAllowedSubTypes(doc.getType(), doc);
+        jg.writeFieldName(ID);
         jg.writeStartArray();
         for (Type subType : subTypes) {
             jg.writeStartObject();
