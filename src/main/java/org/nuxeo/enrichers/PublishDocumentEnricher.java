591dab9a981d7f5d1a5771119d8bf5002324be9d
Vincent Dutat <vdutat@nuxeo.com>
2015-12-31 10:23:28 -0800

    PublishDocumentEnricher

---
+++ src/main/java/org/nuxeo/enrichers/PublishDocumentEnricher.java
@@ -0,0 +1,38 @@
+package org.nuxeo.enrichers;
+
+import static org.nuxeo.ecm.core.io.registry.reflect.Instantiations.SINGLETON;
+import static org.nuxeo.ecm.core.io.registry.reflect.Priorities.REFERENCE;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerator;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.io.marshallers.json.enrichers.AbstractJsonEnricher;
+import org.nuxeo.ecm.core.io.registry.reflect.Setup;
+
+@Setup(mode = SINGLETON, priority = REFERENCE)
+public class PublishDocumentEnricher extends AbstractJsonEnricher<DocumentModel> { 
+ 
+    public static final String NAME = "publishedDocuments";
+    
+    public PublishDocumentEnricher() {
+        super(NAME);
+    }
+ 
+    @Override
+    public void write(JsonGenerator jg, DocumentModel doc) throws IOException {
+      DocumentModelList proxies = doc.getCoreSession().getProxies(doc.getRef(), null);
+      jg.writeFieldName(NAME);
+      jg.writeStartArray();
+      for(DocumentModel proxy : proxies) {
+    	  jg.writeStartObject();
+    	  jg.writeStringField("id", proxy.getId());
+    	  jg.writeStringField("name", proxy.getName());
+    	  jg.writeStringField("path", proxy.getPathAsString());
+    	  jg.writeEndObject();
+      }
+      jg.writeEndArray();
+      jg.flush();
+    }
+}
