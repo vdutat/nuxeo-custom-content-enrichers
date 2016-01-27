2a9db0d273a0d1889ac1a993c32d16ee555ab44f
Vincent Dutat <vdutat@nuxeo.com>
2016-01-26 16:34:41 -0800

    TagsContentEnricher: java class

---
+++ src/main/java/org/nuxeo/enrichers/TagsContentEnricher.java
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     vdutat
+ */
+package org.nuxeo.enrichers;
+
+import static org.nuxeo.ecm.core.io.registry.reflect.Instantiations.SINGLETON;
+import static org.nuxeo.ecm.core.io.registry.reflect.Priorities.REFERENCE;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.codehaus.jackson.JsonGenerator;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.io.marshallers.json.enrichers.AbstractJsonEnricher;
+import org.nuxeo.ecm.core.io.registry.reflect.Setup;
+import org.nuxeo.ecm.platform.tag.Tag;
+import org.nuxeo.ecm.platform.tag.TagService;
+import org.nuxeo.runtime.api.Framework;
+
+@Setup(mode = SINGLETON, priority = REFERENCE)
+public class TagsContentEnricher extends AbstractJsonEnricher<DocumentModel> {
+
+    public static final String NAME = "tags";
+
+    public TagsContentEnricher() {
+        super(NAME);
+    }
+
+    @Override
+    public void write(JsonGenerator jg, DocumentModel doc) throws IOException {
+        TagService ts = Framework.getService(TagService.class);
+        List<Tag> tags = ts.getDocumentTags(doc.getCoreSession(), doc.getId(), null);
+        jg.writeFieldName(NAME);
+        jg.writeStartArray();
+        for(Tag tag : tags) {
+            jg.writeStartObject();
+            jg.writeStringField("label", tag.getLabel());
+            jg.writeStringField("weight", Long.toString(tag.getWeight()));
+            jg.writeEndObject();
+        }
+        jg.writeEndArray();
+        jg.flush();
+    }
+
+}
